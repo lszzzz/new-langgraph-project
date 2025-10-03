1cb947abad22def05f903407de0bab81e7c64de0
samlee <ssli@microware1985.com>
2025-10-03 21:42:29 +0800

    update demo

--- src/demo2.py
+++ src/demo2.py
@@ -99,29 +99,30 @@ graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph = graph_builder.compile()
 
-try:
-    # display(Image(graph.get_graph().draw_mermaid_png()))
-    graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
-except Exception:
-    # This requires some extra dependencies and is optional
-    pass
+# try:
+#     # display(Image(graph.get_graph().draw_mermaid_png()))
+#     graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+# except Exception:
+#     # This requires some extra dependencies and is optional
+#     pass
 
-# def stream_graph_updates(user_input: str):
-#     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-#         for value in event.values():
-#             print("Assistant:", value["messages"][-1].content)
-#
-#
-# while True:
-#     try:
-#         user_input = input("User: ")
-#         if user_input.lower() in ["quit", "exit", "q"]:
-#             print("Goodbye!")
-#             break
-#         stream_graph_updates(user_input)
-#     except:
-#         # fallback if input() is not available
-#         user_input = "What do you know about LangGraph?"
-#         print("User: " + user_input)
-#         stream_graph_updates(user_input)
-#         break
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
